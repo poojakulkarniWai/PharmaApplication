7b425915bd3bddf1b90b44466e2172ccebca47b5
Pooja Kulkarni <pooja.kulkarni@waiin.com>
2024-06-18 18:39:29 +0530

    Product deatils modified

--- src/components/ProductDetailsComponent.js
+++ src/components/ProductDetailsComponent.js
@@ -1,21 +1,10 @@
 // src/components/ProductDetailsComponent.js
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-
-const ProductSubComponent = () => (
-  <div>
-    <h2>Product Sub Component</h2>
-  </div>
-);
 
 const ProductDetailsComponent = () => (
   <div>
     <h1>Product Details Page</h1>
     <p>Details about the product will be shown here.</p>
-    <ul>
-      <li><Link to="/product-details/sub">Product Sub Component</Link></li>
-    </ul>
-    <Route path="/product-details/sub" component={ProductSubComponent} />
   </div>
 );
 
